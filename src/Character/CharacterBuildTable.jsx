35c84def28fc65588f7cfb908c216d67c8aab03f
Liam Ash <liamash3@gmail.com>
2024-11-05 14:39:20 +1100

    add table header rows

--- src/Character/CharacterBuildTable.jsx
+++ src/Character/CharacterBuildTable.jsx
@@ -3,62 +3,60 @@ import React from "react";
 import StatDisplay from "@Stats/StatDisplay";
 import PerksList from "./PerksList";
 import DiceRolls from "@Dice/DiceRolls";
+import "./CharacterBuildTable.css";
 
-const CharacterBuildTable = ({ build }) => (
-  <table>
-    <tbody>
-      <tr>
-        <td>Origin</td>
-        <td>{build.origin}</td>
-      </tr>
-      <tr>
-        <td>Age</td>
-        <td>{build.age}</td>
-      </tr>
-      <tr>
-        <td>Body</td>
-        <td>{build.body}</td>
-      </tr>
-      <tr>
-        <td>Outfit</td>
-        <td>{build.outfit}</td>
-      </tr>
-      <tr>
-        <td>Weapon</td>
-        <td>{build.weapon}</td>
-      </tr>
-      <tr>
-        <td>Specialization</td>
-        <td>{build.specialization}</td>
-      </tr>
-      <tr>
-        <td>Power</td>
-        <td>{build.power}</td>
-      </tr>
-      <tr>
-        <td>Perks</td>
-        <td>
-          <PerksList perks={build.perks} />
-        </td>
-      </tr>
-      <tr>
-        <td>Artifacts</td>
-        <td>{build.ownedArtifacts}</td>
-      </tr>
-      <tr>
-        <td>Stats</td>
-        <td>
-          <StatDisplay stats={build.stats} />
-        </td>
-      </tr>
-      <tr>
-        <td>Dice Rolls</td>
-        <td>
-          <DiceRolls rolls={build.diceRolls} />
-        </td>
-      </tr>
-    </tbody>
-  </table>
-);
+const CharacterBuildTable = ({ build }) => {
+  const rows = [
+    { label: "Origin", value: build.origin },
+    { label: "Age", value: build.age, roll: build.diceRolls[0] },
+    { label: "Body", value: build.body, roll: build.diceRolls[1] },
+    { label: "Outfit", value: build.outfit, roll: build.diceRolls[2] },
+    { label: "Weapon", value: build.weapon, roll: build.diceRolls[3] },
+    {
+      label: "Specialization",
+      value: build.specialization,
+      roll: build.diceRolls[4],
+    },
+    { label: "Power", value: build.power, roll: build.diceRolls[5] },
+    {
+      label: "Perks",
+      value: <PerksList perks={build.perks} />,
+      roll: [
+        build.diceRolls[6],
+        build.diceRolls[7],
+        build.diceRolls[8],
+        build.diceRolls[9],
+        build.diceRolls[10],
+      ].join(", "),
+    },
+    {
+      label: "Artifacts",
+      value: build.ownedArtifacts,
+    },
+    { label: "Stats", value: <StatDisplay stats={build.stats} /> },
+    // { label: "Dice Rolls", value: <DiceRolls rolls={build.diceRolls} /> },
+  ];
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Character</th>
+          <th>Result</th>
+          <th>DiceRoll</th>
+        </tr>
+      </thead>
+      <tbody>
+        {rows.map(({ label, value, roll }, index) => (
+          <tr key={index}>
+            <td>{label}</td>
+            <td>{value}</td>
+            <td>{roll}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
 export default CharacterBuildTable;
